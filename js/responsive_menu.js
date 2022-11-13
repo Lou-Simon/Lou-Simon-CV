d253d6abe1a3b829d7c2b15acdc39aeb27316eff
Lou-Simon <118125741+Lou-Simon@users.noreply.github.com>
2022-11-13 18:52:44 +0100

    Delete js directory

--- js/responsive_menu.js
+++
@@ -1,8 +0,0 @@
-function mobileMenu() {
-  var x = document.getElementById("respNavbar");
-  if (x.className === "navbar") {
-    x.className = " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
